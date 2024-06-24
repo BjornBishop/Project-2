9d70e46902d265e0314a18c4456210efb8865f7f
Bjorn Bishop <bjornbishop@gmail.com>
2024-06-24 21:09:30 +0000

    initial HTML created, structure created in JS and CSS

---
+++ assets/js/javascript.js
@@ -0,0 +1,17 @@
+// Now lets plan out the javascript infrastructure 
+
+//The game should start once all assets are loaded 
+
+//player must be able to chose which weapon to attack with 
+
+//the computer player should chose what weapon to attack with 
+
+//the two options should meet in the middle to be calculated 
+
+//a return value should be given 
+
+//winner nominated 
+
+//life counter
+
+//maybe some cool code if you have time 

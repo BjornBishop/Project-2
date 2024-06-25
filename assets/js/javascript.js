851555d4e1a288d441d92cf981aadc8398494462
Bjorn Bishop <bjornbishop@gmail.com>
2024-06-25 22:51:07 +0000

    rungame function added

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,28 @@
 // Now lets plan out the javascript infrastructure 
 
 //global javascript code 
+document.addEventListener("DOMContentLoaded", function() {
+    let controlButtons = document.getElementsByClassName("control-button")
+
+    for (let conButton of controlButtons) {
+        conButton.addEventListener('click', function() {
+            if (this.getAttribute("data-type") === "higher") {
+                alert(`On a High?`);
+            } else {
+                alert(`Feeling low?`);
+            }
+        })
+    }
+    runGame();
+
+})
+
+//run game loop 
+
+function runGame("higher") {
+    document.getElementById("player-slot").value = 0;
+    document.getElementById("comp-slot").value = 0;
+    document.getElementById("operator").value = 0;
+    document.getElementById("operand").value = 0;
+
+}
